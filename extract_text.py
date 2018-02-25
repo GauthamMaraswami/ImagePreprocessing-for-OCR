5117b02ea64150004ea2612d2198fdc54c4ef163
Satish <satishm752@gmail.com>
2018-02-25 20:13:03 +0530

    Made changes in extract_text.py file. Added to for translating the string returned by pysseract. Translated code is stored in out.txt file

--- extract_text.py
+++ extract_text.py
@@ -1,3 +1,16 @@
+from googletrans import Translator
 import Image
 import pytesseract
-print pytesseract.image_to_string(Image.open('text.jpg'))
+s = [pytesseract.image_to_string(Image.open('text.jpg'),lang='eng')]
+print(s)
+trans =  Translator()
+word_list = trans.translate(s,dest='mr')
+for word in word_list :
+	text=word.text
+	marthi_text = text.encode("utf-8");
+	print(marthi_text)
+	obj_file = open("out.text","w")
+	obj_file.write(marthi_text)
+
+
+
